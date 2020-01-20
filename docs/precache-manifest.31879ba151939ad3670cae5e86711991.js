46d604fbcb9730c247d0b7f8a6acc6b3190342a3
ufrutov <mailluifruti@mail.ru>
2020-01-21 00:46:40 +0200

    Fix GitHub Pages homepage parameter for mvp project

---
+++ docs/precache-manifest.31879ba151939ad3670cae5e86711991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b92ca5375edcd851a917",
+    "url": "/mvp/static/css/main.86a25761.chunk.css"
+  },
+  {
+    "revision": "b92ca5375edcd851a917",
+    "url": "/mvp/static/js/main.b92ca537.chunk.js"
+  },
+  {
+    "revision": "cb0cd2514da0aa545f80",
+    "url": "/mvp/static/js/runtime~main.cb0cd251.js"
+  },
+  {
+    "revision": "ff50fa907cfba0945ac0",
+    "url": "/mvp/static/js/2.ff50fa90.chunk.js"
+  },
+  {
+    "revision": "ae0902b3dcbcb08a37a555d2ee7cb4ce",
+    "url": "/mvp/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4311236d1e320fec98bc2ab49e3a5735.js"
+  "/mvp/precache-manifest.31879ba151939ad3670cae5e86711991.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mvp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
