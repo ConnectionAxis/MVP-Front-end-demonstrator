70826150d9bf5fdb33ae9d21cb5b632177263ea9
ufrutov <mailluifruti@mail.ru>
2020-01-23 10:54:29 +0200

    Updates

---
+++ precache-manifest.b71a6a82589d1193494f0ae1edcfd0a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8c3d8873e5ed94586e4",
+    "url": "/mvp/static/css/main.86a25761.chunk.css"
+  },
+  {
+    "revision": "c8c3d8873e5ed94586e4",
+    "url": "/mvp/static/js/main.c8c3d887.chunk.js"
+  },
+  {
+    "revision": "cb0cd2514da0aa545f80",
+    "url": "/mvp/static/js/runtime~main.cb0cd251.js"
+  },
+  {
+    "revision": "ff50fa907cfba0945ac0",
+    "url": "/mvp/static/js/2.ff50fa90.chunk.js"
+  },
+  {
+    "revision": "7681478aa3cf09a5cb575cee2fd9e611",
+    "url": "/mvp/index.html"
+  }
+];
